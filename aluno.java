5559b6d0995d4b6f43b582e2585d0a7f9f5a14be
UDVSK <142414238+UDVSK@users.noreply.github.com>
2024-03-13 12:33:50 -0300

    Add files via upload

---
+++ aluno.java
@@ -0,0 +1,30 @@
+public class aluno {
+    private String nome, idade, ra, curso;
+
+    public String getCurso() {
+        return curso;
+    }
+    public String getIdade() {
+        return idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getRa() {
+        return ra;
+    }
+
+    
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+}
---
+++ app.java
@@ -0,0 +1,24 @@
+public class app {
+    import java.util.Scanner;
+
+    public static void main (String args[]){
+        Scanner insert = new Scanner(System.in);
+
+       String start = insert.nextLine();
+
+        aluno start = new aluno();
+
+
+
+        Angelica.setNome("Angelica");
+        Angelica.setCurso("ADS");
+        Angelica.setIdade("20");
+        Angelica.setRa("292048");
+
+        System.out.println(Angelica.getNome() + " " + Angelica.getIdade() + " " + Angelica.getRa() + " " + Angelica.getCurso());
+
+
+
+    }
+    
+}
